b27f88de441c7bc1a6afa4a1b19266a6c6ab82e4
RakBok03 <takhtarov2007@gmail.com>
2026-04-14 22:25:54 +0300

    Изменили tours/urls.py new name=tour_list

--- tours/urls.py
+++ tours/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),               # Главная
-    path('tours/', views.tour_list, name='tours'),         # Все туры
-    path('about/', views.about, name='about'),         # О нас
-    path('feedback/', views.feedback_view, name='feedback'), # Форма
+    path('', views.index, name='index'),                      # Главная
+    path('tours/', views.tour_list, name='tour_list'),        # Все туры (исправлено с 'tours' на 'tour_list')
+    path('about/', views.about, name='about'),                # О нас
+    path('feedback/', views.feedback_view, name='feedback'),  # Форма
 ]
