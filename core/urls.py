4d08207cfa0036e80c967a12112c725d454f82c2
RakBok03 <takhtarov2007@gmail.com>
2026-04-20 20:27:46 +0300

    core/urls.py 3

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve # Добавь этот импорт
+from django.urls import re_path # Добавь этот импорт
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tours.urls')),
+    
+    # Прямой проброс медиа-файлов (работает при любом DEBUG)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Убираем проверку if DEBUG, чтобы медиа работали на сервере
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
