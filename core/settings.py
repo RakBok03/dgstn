f335588906d6c27f12ee204a42d521d426401a81
RakBok03 <takhtarov2007@gmail.com>
2026-04-20 18:48:41 +0300

    Add Static_root and Fix paths

--- core/settings.py
+++ core/settings.py
@@ -128,15 +128,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Папка, куда Nginx будет ходить за статикой
+STATIC_ROOT = BASE_DIR / "static_result"
+
+# Папки, где ты хранишь свои стили и картинки дизайна
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# Настройки медиа (фотографии туров)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Папка, где физически лежат загруженные фото
+MEDIA_ROOT = BASE_DIR / "media"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
