1eca249fa00db20e069f2a29fa50dbacc0f660fc
RakBok03 <takhtarov2007@gmail.com>
2026-05-03 08:09:09 +0300

    Improve search snippet metadata and favicon

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import FileResponse, Http404, HttpResponse
 from django.views.static import serve
 from django.urls import re_path
 
 YANDEX_VERIFICATION_FILENAME = "yandex_7f3ca55e413f31a8.html"
+SITE_ICON_DIR = settings.BASE_DIR / "images"
 
 
 def yandex_verification(request):
@@ -13,7 +14,24 @@ def yandex_verification(request):
     return HttpResponse(content, content_type="text/html; charset=UTF-8")
 
 
+def _serve_site_icon(filename, content_type):
+    path = SITE_ICON_DIR / filename
+    if not path.exists():
+        raise Http404
+    return FileResponse(path.open("rb"), content_type=content_type)
+
+
+def favicon_ico(request):
+    return _serve_site_icon("favicon.ico", "image/x-icon")
+
+
+def favicon_png(request):
+    return _serve_site_icon("favicon-120.png", "image/png")
+
+
 urlpatterns = [
+    path("favicon.ico", favicon_ico, name="favicon_ico"),
+    path("favicon.png", favicon_png, name="favicon_png"),
     path(YANDEX_VERIFICATION_FILENAME, yandex_verification, name="yandex_verification"),
     path('admin/', admin.site.urls),
     path('', include('tours.urls')),
--- tours/tests.py
+++ tours/tests.py
@@ -8,3 +8,17 @@ class YandexVerificationTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response["Content-Type"], "text/html; charset=UTF-8")
         self.assertContains(response, "Verification: 7f3ca55e413f31a8")
+
+
+class SiteIconTests(SimpleTestCase):
+    def test_favicon_ico_is_served_from_site_root(self):
+        response = self.client.get("/favicon.ico")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "image/x-icon")
+
+    def test_favicon_png_is_served_from_site_root(self):
+        response = self.client.get("/favicon.png")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "image/png")
